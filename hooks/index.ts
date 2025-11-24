32491c00cbddfdb1a51e06935bc8ee3ec90b3329
meedeley <nicholasaputra77@gmail.com>
2025-11-24 20:46:54 +0700

    feat: new fix

--- hooks/index.ts
+++ hooks/index.ts
@@ -2,7 +2,7 @@
 export { useCart } from './useCart';
 export { useCustomer } from './useCustomer';
 export { useProducts } from './useProducts';
-export { useInvoice } from './useTransaction';
+export { useTransaction } from './useTransaction';
 export { useCustomerHistory } from './useCustomerHistory';
 export { useDataBarang } from './useDataBarang';
 export { useDataJasa } from './useDataJasa';
@@ -12,7 +12,7 @@ export { useDataMekanik } from './useDataMekanik';
 export type { CartItem } from './useCart';
 export type { CustomerInfo } from './useCustomer';
 export type { Service, Part } from './useProducts';
-export type { Invoice } from './useTransaction';
+export type { Transaction } from './useTransaction';
 export type { DataBarang } from './useDataBarang';
 export type { DataJasa } from './useDataJasa';
 export type { DataMekanik } from './useDataMekanik';
